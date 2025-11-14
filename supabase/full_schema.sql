dffbfbbae4c5ad7a0319154000ba8f1f16ae66d6
Ayaan:) <ayaanbeig3030@gmail.com>
2025-11-14 19:44:08 +0400

    fixes :)

--- supabase/full_schema.sql
+++ supabase/full_schema.sql
@@ -235,4 +235,4 @@ $$;
 CREATE TRIGGER on_auth_user_created
   AFTER INSERT ON auth.users
   FOR EACH ROW
-  EXECUTE FUNCTION public.handle_new_user();
+  EXECUTE FUNCTION public.handle_new_user(); 
